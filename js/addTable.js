3f299286097b243ea6c0bcc924eeb8c2de301a51
richahoipen <minhyemen@gmail.com>
2024-11-03 13:49:08 +0700

    Review Web final IUH

---
+++ js/addTable.js
@@ -0,0 +1,31 @@
+function saveToTable(){
+    //get DOM Id from html
+    const fullNameId = "fullName";
+    const telephoneId = "telephone";
+    const dateOfBirthId = "dateOfBirth";
+    const addressId = "address";
+
+    //table render
+    const tableId = "table";
+
+    let fullName = document.getElementById(fullNameId).value;
+    let telephone = document.getElementById(telephoneId).value;
+    let dateOfBirth = document.getElementById(dateOfBirthId).value;
+    let address = document.getElementById(addressId).value;
+
+    let table = document.getElementById(tableId);
+
+    const member = {
+        fullName: fullName.trim(),
+        telephone: telephone.trim(),
+        dateOfBirth: dateOfBirth.trim(),
+        address: address.trim(),
+    };
+
+    let row = table.insertRow();
+    row.insertCell(0).innerHTML = table.rows.length;
+    row.insertCell(1).innerHTML = member.fullName;
+    row.insertCell(2).innerHTML = member.telephone;
+    row.insertCell(3).innerHTML = member.dateOfBirth;
+    row.insertCell(4).innerHTML = member.address;
+}
